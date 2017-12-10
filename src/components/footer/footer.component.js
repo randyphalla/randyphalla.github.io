59dc8ad5266c47eac7b5ecb7bc8b535bc63c810c
Randy Phalla <randy@devlift.io>
2017-12-09 21:24:32 -0500

    Add font-awesome 5, logo and update header & footer component

--- src/components/footer/footer.component.js
+++ src/components/footer/footer.component.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './footer.component.css';
 
 class FooterComponent extends Component {
-  render() {
-    return (
-        <footer id="footer">
-            <h2 className="hidden">Footer</h2>
-            <div className="container">
-                <h3>Footer</h3>
-            </div>
-        </footer>
-    );
-  }
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <footer id="footer">
+                <h2 className="hidden">Footer</h2>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xs-12 col-md-6 social-links">
+                            <ul>
+                                <li>
+                                    <Link to="https://www.behance.net/">
+                                        <i className="fab fa-behance-square"></i>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="https://www.behance.net/">
+                                        <i className="fab fa-github"></i>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="https://www.behance.net/">
+                                        <i className="fab fa-dribbble"></i>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="https://www.behance.net/">
+                                        <i className="fab fa-twitter"></i>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="https://www.behance.net/">
+                                        <i className="fas fa-at"></i>
+                                    </Link>
+                                </li>
+
+                            </ul>
+                        </div>
+                        <div className="col-xs-12 col-md-6 copyright">
+                            <p>Randy Phalla Design</p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
 }
 
 export default FooterComponent;
--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './header.component.css';
+
+import logo from '../../images/logo.png';
 
 class HeaderComponent extends Component {
-  render() {
-    return (
-        <header id="header">
-            <div>
-            </div>
-            <div>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/portfolio">Portfolio</Link>
-                <Link to="/contact">Contact</Link>
-            </div>
-        </header>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+    }
+
+    // TODO: Create Toggle Function
+    toggleMenu() {
+        console.log('ES6');
+    }
+    
+    render() {
+        return (
+            <header id="header">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 col-md-4 logo">
+                            <img src={logo} alt="Randy Phalla Logo"/>
+                        </div>
+                        <div className="col-6 mobile-menu">
+                            <button className="btn" onClick={this.toggleMenu}>
+                                <i className="fas fa-bars 5x"></i>
+                            </button>
+                        </div>
+                        <div className="col-md-8 menu-links">
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/portfolio">Portfolio</Link></li>
+                                <li><Link to="/contact">Contact</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default HeaderComponent;
