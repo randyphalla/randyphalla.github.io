db77b2f628bce96164108f32eb74463dc0cfab20
Randy Phalla <randy@devlift.io>
2017-12-23 21:46:42 -0500

    Update scss

--- src/components/portfolio-page/portfolio-page.js
+++ src/components/portfolio-page/portfolio-page.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react';
+import './portfolio-page.css';
 
 class PortfolioPage extends Component {
     render() {
         return (
-            // TODO: Work on Portfolio Page
-            <section id="about">
-                <h2 className="hidden">About Section</h2>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12"> 
-                            <h3>PROJECT NAME</h3>
-                            <p>Graphic, Web, UI &  Editoral Design and Illustration</p>
+            <section id="portfolio">
+                <h2 className="hidden">Portfolio Section</h2>
+
+                <div className="portfolio-name">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-12"> 
+                                <h3>PROJECT NAME</h3>
+                                <p>Graphic, Web, UI &  Editoral Design and Illustration</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="portfolio-info">
+                    <div className="container">
+                        <div className="row portfolio-card">
+                            <div className="col-12 col-md-6">
+                                <h3>Objective</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat orci ac tortor placerat, a fermentum odio tincidunt. Sed at enim tincidunt mi elementum vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam ac sem porta, convallis lorem a, cursus nulla. In hac habitasse platea dictumst. </p>
+                            </div>
+                            <div className="col-12 col-md-6">
+                                <h3>Mission</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat orci ac tortor placerat, a fermentum odio tincidunt. Sed at enim tincidunt mi elementum vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam ac sem porta, convallis lorem a, cursus nulla. In hac habitasse platea dictumst. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="portfolio-pieces">
+                </div>
+
             </section>
         );
     }
