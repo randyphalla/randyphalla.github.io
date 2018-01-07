dea5da4eae83838d0242d816b832a366f6ac7650
Randy Phalla <randy@devlift.io>
2018-01-06 22:52:18 -0500

    Add mobile links

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -6,27 +6,53 @@ import logo from '../../images/logo.png';
 
 class HeaderComponent extends Component {
     
-    // TODO: Create Toggle Function
+    constructor(props) {    
+        super(props);
+        
+        this.state = {
+            isActive: false
+        }
+    }
+
     toggleMenu() {
-        console.log('ES6');
+        this.setState({
+            isActive: !this.state.isActive
+        });
     }
     
     render() {
+        const { isActive } = this.state;
+
         return (
-            <header id="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6 col-md-4 logo">
-                            <Link to="/">
-                                <img src={logo} alt="Randy Phalla Logo"/>
-                            </Link>
+            <div>
+                <header id="header">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-6 col-md-4 logo">
+                                <Link to="/">
+                                    <img src={logo} alt="Randy Phalla Logo"/>
+                                </Link>
+                            </div>
+                            <div className="col-6 mobile-menu">
+                                <button className="btn" aria-label="Toggle Menu" onClick={this.toggleMenu.bind(this)}>
+                                    <i className="fas fa-bars 5x"></i>
+                                </button>
+                            </div>
+                            <div className="col-md-8 menu-links">
+                                <ul>
+                                    <li><Link to="/">Home</Link></li>
+                                    <li><Link to="/about">About</Link></li>
+                                    <li><Link to="/portfolio">Portfolio</Link></li>
+                                    <li><Link to="/contact">Contact</Link></li>
+                                </ul>
+                            </div>
                         </div>
-                        <div className="col-6 mobile-menu">
-                            <button className="btn"  aria-label="Toggle Menu" onClick={this.toggleMenu}>
-                                <i className="fas fa-bars 5x"></i>
-                            </button>
-                        </div>
-                        <div className="col-md-8 menu-links">
+                    </div>                
+                </header>
+                {
+                    isActive &&
+                    (
+                        <div className="mobile-links">
                             <ul>
                                 <li><Link to="/">Home</Link></li>
                                 <li><Link to="/about">About</Link></li>
@@ -34,9 +60,9 @@ class HeaderComponent extends Component {
                                 <li><Link to="/contact">Contact</Link></li>
                             </ul>
                         </div>
-                    </div>
-                </div>
-            </header>
+                    )
+                }
+            </div>
         );
     }
 }
