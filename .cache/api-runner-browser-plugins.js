6c41b90981157821fd4cb498918df13071ef2143
Randy Phallla <randyphalla@gmail.com>
2019-02-24 18:59:48 -0500

    remove files and add gatsbyjs

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/randyphallla/Documents/GitHub/randyphalla.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
--- dev/gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var gzip = require('gulp-gzip');
-
-gulp.task('compress', function() {
-    return gulp.src(['./build/**/*.{js,css,html,svg,eot,ttf,woff,woff2}'])
-        .pipe(gzip())
-        .pipe(gulp.dest('./build'));
-});
