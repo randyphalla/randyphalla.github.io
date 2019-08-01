af25c0462ef9beac85ebf19a24605ccd1da74f78
Randy Phallla <randyphalla@gmail.com>
2019-07-31 21:09:13 -0400

    testing webhooks

--- src/theme/banner.style.js
+++ src/theme/banner.style.js
@@ -1,14 +1,22 @@
 import styled from "styled-components";
 
 const BannerContainer = styled.div `
-    position: relative;
-    height: 585px;
     display: flex;
-
     align-items: center;
+    justify-content: center;
+    padding: 60px 0;
+    
     text-align: center;
     
     background-color: var(--red-color);
+    transition: padding 0.3s ease;
+
+    @media screen and (min-width: 576px) {
+        position: relative;
+        /* height: 585px; */
+        padding-top: 200px;
+        padding-bottom: 350px;
+    }
 `;
 
 const BannerTitle = styled.h3 `
@@ -16,46 +24,53 @@ const BannerTitle = styled.h3 `
     padding-bottom: 10px;
 
     color: var(--white-color);
-    font-size: 60px;
+    font-size: 30px;
     font-family: 'Montserrat', sans-serif;
     font-weight: 700;
-    line-height: 66px;
+
+    @media screen and (min-width: 576px) {
+        font-size: 60px;
+        line-height: 66px;
+    }
 `;
 
 const BannerDescription = styled.p `
     margin: 0;
         
     color: var(--white-color);
-    font-size: 18px;
+    font-size: 14px;
     font-family: 'Raleway', sans-serif;
     font-weight: 500;
     letter-spacing: 0.5px;
     line-height: 24px;
+
+    @media screen and (min-width: 576px) {
+        font-size: 18px;
+        line-height: 28px;
+    }
 `;
 
 const BannerBottomBackground = styled.div`
-    position: absolute;
-    bottom: -5px;
-    left: 0;
-    height: inherit;
-    width: 100%;
-    
-    img {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-    }
+    display: none;
 
-    @media (min-width: 576px) {
+    @media screen and (min-width: 576px) {
+        display: block;
+        position: absolute;
+        bottom: -5px;
+        left: 0;
+        height: inherit;
+        width: 100%;
 
         img {
+            position: absolute;
+            bottom: 0;
+            left: 0;
             height: 150px;
+            width: 100%;
         }
-
     }
 
-    @media (min-width: 768px) {
+    @media screen and (min-width: 768px) {
 
         img {
             height: 200px;
