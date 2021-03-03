b6304b76033c2513be5f32ee8ac8763ed0d40efa
Randy Phalla <randyphalla@gmail.com>
2021-03-03 16:05:33 -0500

    update packages

--- src/components/banner/banner.style.tsx
+++ src/components/banner/banner.style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const BannerSection = styled.div`
+export const BannerSection = styled.section`
   display: flex;
   align-items: center;
   justify-content: center;
