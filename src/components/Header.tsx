d249343a7f963091c634517cfbefe3414541f04c
Randy Phalla <randyphalla@gmail.com>
2022-09-26 15:49:37 -0400

    Update styling, copy and lighthouse speed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="">
       <h2 className="hidden">Header</h2>
-      <div className="max-w-5xl mx-auto pt-4 pb-4 px-4 ">
+      <div className="max-w-5xl mx-auto pt-4 pb-4 px-4">
         <div>
           <img className="" src="" alt="Randy Phalla" />
         </div>
@@ -12,16 +12,19 @@ const Header = () => {
               <a href="#">Home</a>
             </li>
             <li>
-              <a href="#aboutMe">About</a>
+              <a href="#aboutMe" aria-label="Scroll to about section">About</a>
             </li>
             <li>
-              <a href="#work">Work Experience</a>
+              <a href="#skills" aria-label="Scroll to skills section">Skills</a>
             </li>
             <li>
-              <a href="#education">Education</a>
+              <a href="#work" aria-label="Scroll to work Experiences section">Work Experiences</a>
             </li>
             <li>
-              <a href="#projects">Projects</a>
+              <a href="#education" aria-label="Scroll to education section">Education</a>
+            </li>
+            <li>
+              <a href="#projects" aria-label="Scroll to projects section">Projects</a>
             </li>
           </ul>
         </nav>
