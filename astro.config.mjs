80a80d84f9b8fb05d4618238ff5d039799938e54
Randy Phalla <randyphalla@gmail.com>
2022-09-28 19:35:41 -0400

    Remove package and update styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
     tailwind(),
-    image()
   ]
 });
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,53 +5,54 @@ const Footer = () => {
     <footer className="max-w-5xl mx-auto py-10 px-4">
       <h2 className="hidden">Footer</h2>
 
-      <div className="flex justify-center md:justify-between items-center flex-col md:flex-row">
+      {/* flex justify-center md:justify-between items-center flex-col md:flex-row */}
+      <div className="flex md:grid justify-center md:justify-between items-center flex-col md:flex-row md:grid-cols-3">
 
         <img className="w-16" src="public/randyphalla-icon.png" alt="Randy Phalla" />
 
-        <ul className="mt-5 mb-5 md:mt-0 md:mb-0 flex justify-center md:justify-evenly items-center md:items-start flex-col md:flex-row">
+        <ul className="mt-5 mb-5 md:mt-0 md:mb-0 flex justify-center md:justify-start items-center md:items-start flex-col md:flex-row">
 
           <li className="mt-2 mb-2 md:mt-0 md: mb-0 ml-2 mr-2 w-fit">
             <a href="https://github.com/randyphalla" target="_blank" aria-label="Redirect to Github">
-              <IoLogoGithub className="text-4xl md:text-2xl hover:text-gray-600" />
+              <IoLogoGithub className="text-4xl md:text-2xl hover:text-gray-600 transition-all" />
             </a>
           </li>
 
           <li className="mt-2 mb-2 md:mt-0 md: mb-0 ml-2 mr-2 w-fit">
             <a href="https://www.linkedin.com/in/randyphalla/" target="_blank" aria-label="Redirect to Linkedin">
-              <IoLogoLinkedin className="text-4xl md:text-2xl hover:text-gray-600" />
+              <IoLogoLinkedin className="text-4xl md:text-2xl hover:text-gray-600 transition-all" />
             </a>
           </li>
 
           <li className="mt-2 mb-2 md:mt-0 md: mb-0 ml-2 mr-2 w-fit">
             <a href="https://dribbble.com/randyphalla" target="_blank" aria-label="Redirect to Dribbble">
-              <IoLogoDribbble className="text-4xl md:text-2xl hhover:text-gray-600" />
+              <IoLogoDribbble className="text-4xl md:text-2xl hover:text-gray-600 transition-all" />
             </a>
           </li>
 
           <li className="mt-2 mb-2 md:mt-0 md: mb-0 ml-2 mr-2 w-fit">
             <a href="https://www.behance.net/randyphalla" target="_blank" aria-label="Redirect to Behance">
-              <IoLogoBehance className="text-4xl md:text-2xl hover:text-gray-600" />
+              <IoLogoBehance className="text-4xl md:text-2xl hover:text-gray-600 transition-all" />
             </a>
           </li>
 
           <li className="mt-2 mb-2 md:mt-0 md: mb-0 ml-2 mr-2 w-fit">
             <a href="#" aria-label="Open email">
-              <IoMail className="text-4xl md:text-2xl hover:text-gray-600" />
+              <IoMail className="text-4xl md:text-2xl hover:text-gray-600 transition-all" />
             </a>
           </li>
 
         </ul>
 
         <div className="">
-          <p className="text-sm">Designed and Built by <span className="font-bold">Randy Phalla</span></p>
-          <p className="text-sm">Built with <a className="font-bold hover:text-gray-600" href="https://astro.build/" target="_blank" aria-label="Redirect to Astro">Astro</a>, <a className="font-bold hover:text-gray-600" href="https://tailwindcss.com/" target="_blank" aria-label="Redirect to Tailwind">Tailwind</a>, and <a className="font-bold hover:text-gray-600" href="https://reactjs.org/" target="_blank" aria-label="Redirect to React">React</a></p>
+          <p className="text-sm md:text-base">Designed and Built by <span className="font-bold">Randy Phalla</span></p>
+          <p className="text-sm md:text-base">Built with <a className="font-bold hover:text-gray-600" href="https://astro.build/" target="_blank" aria-label="Redirect to Astro">Astro</a>, <a className="font-bold hover:text-gray-600" href="https://tailwindcss.com/" target="_blank" aria-label="Redirect to Tailwind">Tailwind</a>, and <a className="font-bold hover:text-gray-600" href="https://reactjs.org/" target="_blank" aria-label="Redirect to React">React</a></p>
         </div>
 
       </div>
 
-      <div className="mt-5 text-left">
-        <p className="text-sm">&copy;2022 Randy Phalla</p>
+      <div className="mt-7 text-center md:text-left">
+        <p className="text-sm md:text-base">&copy;2022 Randy Phalla</p>
       </div>
 
     </footer>
