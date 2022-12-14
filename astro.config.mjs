3224d1dd9ae97d0b4515d586dfb04cf87370c0d5
Randy Phalla <randyphalla@gmail.com>
2022-12-13 23:22:35 -0500

    Remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
-// If you wanna use edge-function, comment out import netlify from "@astrojs/netlify/functions"
-// import netlify from '@astrojs/netlify/edge-functions';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://randyphalla.github.io',
   integrations: [
