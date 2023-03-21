826d7b322f216262a75420d07b5e273cfa93680a
Randy Phalla <randyphalla@gmail.com>
2023-03-21 18:53:48 -0400

    Add partytown for google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://randyphalla.github.io/',
   integrations: [react(), tailwind(), sitemap({
     customPages: ['https://randyphalla.github.io', 'https://symphonious-torte-5a2754.netlify.app/']
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
   })],
   server: {
     port: 4200
-  },
+  }
 });
