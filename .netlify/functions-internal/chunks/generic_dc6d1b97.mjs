6f0b79614255618fea3e4da818d2b885745d9153
Randy Phalla <randyphalla@gmail.com>
2024-01-27 18:51:11 -0500

    delete .netlify/functions-internal

--- .netlify/functions-internal/chunks/generic_dc6d1b97.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/generic_e8e1870b.mjs').then(n => n.g);
-
-export { page };
--- .netlify/functions-internal/chunks/index_15c74bd7.mjs
+++
@@ -1,8 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-import 'react';
-import 'react-dom/server';
-
-const page = () => import('./pages/index_9e596b47.mjs');
-
-export { page };
