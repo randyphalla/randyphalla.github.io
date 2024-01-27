d3a4c20564f072e45585af99347385f383294101
Randy Phalla <randyphalla@gmail.com>
2024-01-27 18:44:32 -0500

    comment out partytown and netlify in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
-import partytown from "@astrojs/partytown";
-import netlify from '@astrojs/netlify/functions';
+// import partytown from "@astrojs/partytown";
+// import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,12 +16,12 @@ export default defineConfig({
     sitemap({
       customPages: ['https://randyphalla.github.io', 'https://symphonious-torte-5a2754.netlify.app/']
     }),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    })
+    // partytown({
+    //   config: {
+    //     forward: ["dataLayer.push"],
+    //   },
+    // }),
   ],
-  output: 'server',
-  adapter: netlify(),
+  // output: 'server',
+  // adapter: netlify(),
 });
