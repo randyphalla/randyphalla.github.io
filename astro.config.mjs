9aac1a76a7792a9c508b0d2283ca67b336cb3154
Randy Phalla <randyphalla@gmail.com>
2025-02-21 02:02:29 -0500

    fix config and remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://randyphalla.github.io',
-  base: 'https://github.com/randyphalla/randyphalla.github.io',
   integrations: [
     react({
       include: ['**/react/*']
