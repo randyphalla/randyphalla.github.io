f8e79172860f8f778e18dff098aef0e3b32e0314
Randy Phalla <randyphalla@gmail.com>
2025-04-01 14:07:49 -0400

    remove comment

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -40,7 +40,6 @@ const Portfolio = (props: PortfolioProps) => {
                     ? <img className="w-full h-full" src={portfolio.imgSrc} alt={portfolio.imgSrcAlt} loading="lazy" />
                     : <div className="w-full h-full bg-white" />}
                   <h4 className="mt-4 text-white dark:text-white font-bold text-base md:text-lg leading-7 md:leading-8 text-left">{portfolio.title}</h4>
-                  {/* {portfolio.type && (<p className="text-gray-400 dark:text-gray-200 text-sm text-left">{portfolio.type}</p>)} */}
                   {portfolio.projectType && (<p className="text-gray-400 dark:text-gray-200 text-sm text-left">{portfolio.projectType}</p>)}
                 </div>
               </button>
